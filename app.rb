7b32c7e040dcf4aa0bfade16791a1f1048b9eb71
Kat Maldon <katharinamaldon@gmail.com>
2019-06-06 19:00:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Kat."
+  end
+
+  get '/hometown' do
+    "My hometown is Hamburg."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Our love is easy'."
+  end
 end
